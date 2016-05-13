cff4d4d2f636cf49654d3c923279008ebe6513ef
TimariuCristian <cristian.timariu@gmail.com>
2016-05-13 04:51:11 +0300

    Added game rules in Board class.

--- java-2016/TicTacToe Game/src/game/java/Board.java
+++ java-2016/TicTacToe Game/src/game/java/Board.java
@@ -21,4 +21,45 @@ public class Board {
 			}
 		}
 	}
+
+	/**
+	 * Determines if there is a winner.
+	 * 
+	 * @param mark
+	 *            (X, O, Empty)
+	 * @return True, if someone wins.
+	 */
+	public boolean hasWon(Mark mark) {
+		return (// search for 3 in a row.
+		cells[0][0].content.equals(mark) && cells[0][1].equals(mark) && cells[0][2].equals(mark)
+				|| cells[1][0].content.equals(mark) && cells[1][1].equals(mark) && cells[1][2].equals(mark)
+				|| cells[2][0].content.equals(mark) && cells[2][1].equals(mark) && cells[2][2].equals(mark)
+				// search for 3 in a col.
+				|| cells[0][0].content.equals(mark) && cells[1][0].equals(mark) && cells[2][0].equals(mark)
+				|| cells[0][1].content.equals(mark) && cells[1][1].equals(mark) && cells[2][1].equals(mark)
+				|| cells[0][2].content.equals(mark) && cells[1][2].equals(mark) && cells[2][2].equals(mark)
+				// search for 3 in the diagonal
+				|| cells[0][0].content.equals(mark) && cells[1][1].equals(mark) && cells[2][2].equals(mark)
+				// search for 3 in the opp-diagonal
+				|| cells[0][2].content.equals(mark) && cells[1][1].equals(mark) && cells[2][0].equals(mark));
+	}
+
+	/**
+	 * Boolean method to determine if the game is "draw".
+	 * 
+	 * @return True, if there are no more moves. False, if finds empty cells.
+	 */
+	public boolean isDraw() {
+		for (int row = 0; row < 3; ++row) {
+			for (int col = 0; col < 3; ++col) {
+				// Goes through board and search if there are empty cells
+				// left.
+				if (cells[row][col].content == Mark.EMPTY) {
+					// If one cell is empty, game continues.
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 }
